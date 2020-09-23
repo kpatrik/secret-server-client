ec620b2a0837af2d3e1b7bf6ba2df18e70035611
Patrik Koleszár <patrik.koleszar@emarsys.com>
2020-09-23 22:38:57 +0200

    feat(secret-server-client): alert on error

--- src/lib/clients/secret-server-client.js
+++ src/lib/clients/secret-server-client.js
@@ -4,11 +4,15 @@ export class SecretServerClient {
   static async postSecret(secret, expireAfterViews, expireAfter) {
     return await axios.post('http://localhost:3010/api/secret', {
       secret, expireAfterViews, expireAfter
+    }).catch((error) => {
+      alert(error);
     })
   }
 
   static async getSecret(hash) {
-    return await axios.get(`http://localhost:3010/api/secret/${hash}`);
+    return await axios.get(`http://localhost:3010/api/secret/${hash}`).catch((error) => {
+      alert(error);
+    });
   }
 }
 
